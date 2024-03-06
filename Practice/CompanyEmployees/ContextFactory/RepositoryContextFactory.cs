f0fc83cd8494228a45e0e454e4fdd2f399ac7311
Arslan Ahmad <ars.ahm6@gmail.com>
2024-03-06 10:34:05 +0500

    Ready for migration.

---
+++ Practice/CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory
+{
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("sqlConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(connectionString);
+
+            var repoContext = new RepositoryContext(optionsBuilder.Options);
+            return repoContext;
+        }
+    }
+}
